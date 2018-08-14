70fcd24c65aa2850221985e2cdac1c85969e692d
Pedro Northon Nobile <nobile.pedro@gmail.com>
2018-08-13 21:17:56 -0300

    Segundo commit com imagens dos dados sendo mostradas a cada sorteio.

--- app/src/main/java/br/edu/ifsp/scl/sdm/dicesdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dicesdm/MainActivity.java
@@ -3,6 +3,7 @@ package br.edu.ifsp.scl.sdm.dicesdm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -11,8 +12,9 @@ public class MainActivity extends AppCompatActivity {
     // Gerador de números randômicos usado para simular o lançamento do dado
     private Random geradorRandomico;
 
-    // Referência para resultadoTextView do arquivo de layout
+    // Referências para as Views do arquivo de layout
     private TextView resultadoTextView;
+    private ImageView resultadoImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +24,9 @@ public class MainActivity extends AppCompatActivity {
         // Após a criação da tela
         geradorRandomico = new Random(System.currentTimeMillis());
 
-        // Recuperando referência para o resultadoTextView do arquivo de layout
+        // Recuperando referência para as Views do arquivo de layout
         resultadoTextView = findViewById(R.id.resultadoTextView);
+        resultadoImageView = findViewById(R.id.resultadoImageView);
     }
 
     public void jogarDado(View view){
@@ -31,6 +34,26 @@ public class MainActivity extends AppCompatActivity {
             int resultado = geradorRandomico.nextInt(6) + 1;
 
             resultadoTextView.setText(getString(R.string.a_face_sorteada_foi) + resultado);
+            switch (resultado) {
+                case 1:
+                    resultadoImageView.setImageResource(R.drawable.dice_1);
+                    break;
+                case 2:
+                    resultadoImageView.setImageResource(R.drawable.dice_2);
+                    break;
+                case 3:
+                    resultadoImageView.setImageResource(R.drawable.dice_3);
+                    break;
+                case 4:
+                    resultadoImageView.setImageResource(R.drawable.dice_4);
+                    break;
+                case 5:
+                    resultadoImageView.setImageResource(R.drawable.dice_5);
+                    break;
+                case 6:
+                    resultadoImageView.setImageResource(R.drawable.dice_6);
+                    break;
+            }
         }
     }
 }
