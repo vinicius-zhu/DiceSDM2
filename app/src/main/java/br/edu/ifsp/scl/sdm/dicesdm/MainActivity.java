b8d6c8046b12430a01e23a6a756c0498a86302ff
Pedro Northon Nobile <nobile.pedro@gmail.com>
2018-08-13 22:00:47 -0300

    Terceiro commit com a possibilidade de escolha de quantos dados serão lançados.

--- app/src/main/java/br/edu/ifsp/scl/sdm/dicesdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dicesdm/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     // Referências para as Views do arquivo de layout
     private TextView resultadoTextView;
     private ImageView resultadoImageView;
+    private Spinner numDicesSpinner;
+    private ImageView resultado2ImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,33 +30,50 @@ public class MainActivity extends AppCompatActivity {
         // Recuperando referência para as Views do arquivo de layout
         resultadoTextView = findViewById(R.id.resultadoTextView);
         resultadoImageView = findViewById(R.id.resultadoImageView);
+        numDicesSpinner = findViewById(R.id.numDicesSpinner);
+        resultado2ImageView = findViewById(R.id.resultado2imageView);
     }
 
     public void jogarDado(View view){
         if (view.getId() == R.id.jogarDadoButton) {
-            int resultado = geradorRandomico.nextInt(6) + 1;
-
-            resultadoTextView.setText(getString(R.string.a_face_sorteada_foi) + resultado);
-            switch (resultado) {
-                case 1:
-                    resultadoImageView.setImageResource(R.drawable.dice_1);
-                    break;
-                case 2:
-                    resultadoImageView.setImageResource(R.drawable.dice_2);
-                    break;
-                case 3:
-                    resultadoImageView.setImageResource(R.drawable.dice_3);
-                    break;
-                case 4:
-                    resultadoImageView.setImageResource(R.drawable.dice_4);
-                    break;
-                case 5:
-                    resultadoImageView.setImageResource(R.drawable.dice_5);
-                    break;
-                case 6:
-                    resultadoImageView.setImageResource(R.drawable.dice_6);
-                    break;
+            int numDices = Integer.parseInt(numDicesSpinner.getSelectedItem().toString());
+            if (numDices == 2) {
+                resultado2ImageView.setVisibility(View.VISIBLE);
             }
+            else{
+                resultado2ImageView.setVisibility(View.GONE);
+            }
+
+            String resultadoText = "";
+            for (int i = 1; i <= numDices; i++) {
+                int resultado = geradorRandomico.nextInt(6) + 1;
+
+                resultadoText += resultado + " ";
+
+                ImageView imageView = i == 1? resultadoImageView : resultado2ImageView;
+                switch (resultado) {
+                    case 1:
+                        imageView.setImageResource(R.drawable.dice_1);
+                        break;
+                    case 2:
+                        imageView.setImageResource(R.drawable.dice_2);
+                        break;
+                    case 3:
+                        imageView.setImageResource(R.drawable.dice_3);
+                        break;
+                    case 4:
+                        imageView.setImageResource(R.drawable.dice_4);
+                        break;
+                    case 5:
+                        imageView.setImageResource(R.drawable.dice_5);
+                        break;
+                    case 6:
+                        imageView.setImageResource(R.drawable.dice_6);
+                        break;
+                }
+            }
+
+            resultadoTextView.setText(getString(R.string.a_face_sorteada_foi) + resultadoText);
         }
     }
 }
